75bc8912b0c3b370a87bb1463540d0f2608f20a6
Jiayin Tang <jt2823@columbia.edu>
2016-10-04 19:43:07 -0400

    added basic log in and signup page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-        return "Welcome!"
+        return render_template('index.html')
 
 if __name__ == "__main__":
         app.run()
+
+@app.route('/showSignUp')
+def showSignUp():
+        return render_template('signup.html')
+
